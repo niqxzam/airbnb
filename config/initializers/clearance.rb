3c88eb9c1b4585c5ac7fae49cdf4d121694a1ddc
niqxzam <shah_nidz@yahoo.com>
2018-04-23 18:23:29 +0800

    Sign out enabled

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -5,8 +5,8 @@ Clearance.configure do |config|
   config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
   config.cookie_name = "remember_token"
   config.cookie_path = "/"
-  config.routes = true
-  config.httponly = false
+  config.routes = false
+  config.httponly = true
   config.mailer_sender = "reply@example.com"
   config.password_strategy = Clearance::PasswordStrategies::BCrypt
   config.redirect_url = "/"
