7a9c5074c31ac4d6a6d4490edfab046bba0bd5da
niqxzam <shah_nidz@yahoo.com>
2018-04-24 15:16:42 +0800

    Google log-in completed.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+ provider :google_oauth2, '754734930325-8rdj57uq9lmfih41eion0g95b8jjf377.apps.googleusercontent.com' , 'EznjEHrs7RHHncha_rrReG9N'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424035114) do
+ActiveRecord::Schema.define(version: 20180424064411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.string   "uid"
+    t.string   "token"
+    t.string   "provider"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_authentications_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                     null: false
@@ -28,4 +38,5 @@ ActiveRecord::Schema.define(version: 20180424035114) do
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
   end
 
+  add_foreign_key "authentications", "users"
 end
